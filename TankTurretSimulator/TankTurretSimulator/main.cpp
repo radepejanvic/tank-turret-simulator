703321487a08a2457a839900fe39594f4f2ad80a
Rade Pejanović <radepejanovic@gmail.com>
2024-11-22 16:39:32 +0100

    feat: add hello window

---
+++ TankTurretSimulator/TankTurretSimulator/main.cpp
@@ -0,0 +1,41 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+	if (!glfwInit()) {
+		return 1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(1600, 900, "Hello, window!", glfwGetPrimaryMonitor(), NULL);
+
+	if (window == NULL) {
+		glfwTerminate();
+		return 2; 
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK) {
+		glfwTerminate();
+		return 3; 
+	}
+
+	glClearColor(0.25, 0.0, 0.25, 1);
+
+	while (!glfwWindowShouldClose(window)) {
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+			glfwSetWindowShouldClose(window, true);
+		}
+
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
