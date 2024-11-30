c88fedd0a49d57be25572ddb18086cc4925bb0d2
Rade Pejanović <radepejanovic@gmail.com>
2024-11-30 18:50:44 +0100

    fix: remove comments

--- TankTurretSimulator/TankTurretSimulator/text_handler.cpp
+++ TankTurretSimulator/TankTurretSimulator/text_handler.cpp
@@ -46,17 +46,16 @@ int TextHandler::loadFont(const char* fontPath)
 		return -1;
 	}
 
-	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // disable byte-alignment restriction
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); 
 
 	for (unsigned char c = 0; c < 128; c++)
 	{
-		// load character glyph 
 		if (FT_Load_Char(face, c, FT_LOAD_RENDER))
 		{
 			std::cout << "ERROR::FREETYPE: Failed to load Glyph" << std::endl;
 			continue;
 		}
-		// generate texture
+		
 		unsigned int texture;
 		glGenTextures(1, &texture);
 		glBindTexture(GL_TEXTURE_2D, texture);
@@ -71,12 +70,12 @@ int TextHandler::loadFont(const char* fontPath)
 			GL_UNSIGNED_BYTE,
 			face->glyph->bitmap.buffer
 		);
-		// set texture options
+		
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		// now store character for later use
+		
 		Character character = {
 			texture,
 			glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
@@ -94,13 +93,11 @@ int TextHandler::loadFont(const char* fontPath)
 
 void TextHandler::renderText(std::string text, float x, float y, float scale, glm::vec3 color)
 {
-	// activate corresponding render state	
 	shader.use();
 	glUniform3f(glGetUniformLocation(0, "textColor"), color.x, color.y, color.z);
 	glActiveTexture(GL_TEXTURE0);
 	glBindVertexArray(VAO);
 
-	// iterate through all characters
 	std::string::const_iterator c;
 	for (c = text.begin(); c != text.end(); c++)
 	{
@@ -111,7 +108,7 @@ void TextHandler::renderText(std::string text, float x, float y, float scale, gl
 
 		float w = ch.Size.x * scale;
 		float h = ch.Size.y * scale;
-		// update VBO for each character
+		
 		float vertices[6][4] = {
 			{ xpos,     ypos + h,   0.0f, 0.0f },
 			{ xpos,     ypos,       0.0f, 1.0f },
@@ -121,16 +118,16 @@ void TextHandler::renderText(std::string text, float x, float y, float scale, gl
 			{ xpos + w, ypos,       1.0f, 1.0f },
 			{ xpos + w, ypos + h,   1.0f, 0.0f }
 		};
-		// render glyph texture over quad
+		
 		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
-		// update content of VBO memory
+		
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		// render quad
+		
 		glDrawArrays(GL_TRIANGLES, 0, 6);
-		// now advance cursors for next glyph (note that advance is number of 1/64 pixels)
-		x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64)
+		
+		x += (ch.Advance >> 6) * scale;
 	}
 	glBindVertexArray(0);
 	glBindTexture(GL_TEXTURE_2D, 0);
